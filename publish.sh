bd2840d1cd602fb2a5424d319093c78183d0779e
khai96_ <hvksmr1996@gmail.com>
2020-02-26 22:06:46 +0700

    Improve publish.sh

--- publish.sh
+++ publish.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/env sh
+cd "$(dirname "$0")" || exit $?
 echo '>>> master'
 git checkout master || exit $?
 git push origin master || exit $?
+echo ':: generate'
+deno --allow-all cmd/generate-index-page.ts
 echo '>>> gh-pages'
 git checkout gh-pages || exit $?
 git merge master || exit $?
