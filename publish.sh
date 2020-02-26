158fec652d2e02daa0ae899ae40e5dae45081eb7
khai96_ <hvksmr1996@gmail.com>
2020-02-26 22:01:55 +0700

    Add messages

--- publish.sh
+++ publish.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env sh
+echo '>>> master'
 git checkout master || exit $?
 git push origin master || exit $?
+echo '>>> gh-pages'
 git checkout gh-pages || exit $?
 git merge master || exit $?
 git push origin gh-pages || exit $?
