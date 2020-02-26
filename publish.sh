482764cdc9b575d280c0feed4d227af8845af66f
khai96_ <hvksmr1996@gmail.com>
2020-02-26 22:09:22 +0700

    Make some spaces

--- publish.sh
+++ publish.sh
@@ -1,15 +1,20 @@
 #! /usr/bin/env sh
+
 cd "$(dirname "$0")" || exit $?
+
 echo '>>> master'
 git checkout master || exit $?
+
 echo ':: generate'
 deno --allow-all cmd/generate-index-page.ts
 git add tree.html || exit $?
 git commit -m 'Update tree.html' || exit $?
 git push origin master || exit $?
+
 echo '>>> gh-pages'
 git checkout gh-pages || exit $?
 git merge master || exit $?
 git push origin gh-pages || exit $?
+
 echo '>>> master'
 exec git checkout master
