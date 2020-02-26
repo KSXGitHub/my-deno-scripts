11315bd698b8f8c4149213b4b2392ffbb4368a08
khai96_ <hvksmr1996@gmail.com>
2020-02-26 22:08:44 +0700

    Rearrange commands

--- publish.sh
+++ publish.sh
@@ -2,11 +2,11 @@
 cd "$(dirname "$0")" || exit $?
 echo '>>> master'
 git checkout master || exit $?
-git push origin master || exit $?
 echo ':: generate'
 deno --allow-all cmd/generate-index-page.ts
 git add tree.html || exit $?
 git commit -m 'Update tree.html'
+git push origin master || exit $?
 echo '>>> gh-pages'
 git checkout gh-pages || exit $?
 git merge master || exit $?
