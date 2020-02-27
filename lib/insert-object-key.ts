6a45940a3ad3d16a130470e98d2948b0c812066f
khai96_ <hvksmr1996@gmail.com>
2020-02-27 12:53:37 +0700

    Provide when constructors

--- lib/insert-object-key.ts
+++ lib/insert-object-key.ts
@@ -5,6 +5,10 @@ interface ConditionParam<Object> {
   readonly valueAfter: Object[keyof Object]
 }
 
+interface WhenFn<Object> {
+  (param: ConditionParam<Object>): boolean
+}
+
 export function insertObjectKey<
   Object,
   Key extends string | number,
@@ -13,7 +17,7 @@ export function insertObjectKey<
   object: Object,
   key: Key,
   value: Value,
-  when: (param: ConditionParam<Object>) => boolean
+  when: WhenFn<Object>
 ): Object & {
   [_ in Key]: Value
 } {
@@ -42,3 +46,11 @@ export function insertObjectKey<
 
   return { ...object, [key]: value } as any
 }
+
+type ConPrmAny = ConditionParam<any>
+type WhenKey = <Object> (key: keyof Object) => WhenFn<Object>
+type WhenValue = <Object> (key: Object[keyof Object]) => WhenFn<Object>
+export const BEFORE_KEY: WhenKey = k => (x: ConPrmAny) => x.keyAfter === k
+export const AFTER_KEY: WhenKey = k => (x: ConPrmAny) => x.keyBefore === k
+export const BEFORE_VALUE: WhenValue = v => (x: ConPrmAny) => x.valueAfter === v
+export const AFTER_VALUE: WhenValue = v => (x: ConPrmAny) => x.valueAfter === v
